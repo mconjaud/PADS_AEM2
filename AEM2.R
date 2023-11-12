274eea0ffa1dab1cf19ebac3309eae670748c15b
MMCN <michelmconjaud@gmail.com>
2023-11-12 11:20:15 -0300

    Base de dados direto do Git
    Leitura da base de dados produtividade_policial realizada diretamente do csv no repositório do Github.
    Atualização no código do R removendo algumas colunas desnecessárias.

--- AEM2.R
+++ AEM2.R
@@ -8,9 +8,9 @@ library(ggrepel)
 library(skimr)
 library(dplyr)
 
-dados <- read_xlsx("C:/Users/miche/Desktop/Aulas/Insper/AEM2/AEM_Atividade02/produtividade_policial.xlsx")
 
-view(dados)
+
+dados <- read.csv2("https://raw.githubusercontent.com/mconjaud/PADS_AEM2/main/produtividade_policial.csv")
 
 dados %>% 
   sample_n(10)
@@ -19,29 +19,13 @@ dados <- dados %>%
   rename_with(~ stri_trans_general(.x, "Latin-ASCII") %>% 
                 tolower())
 
-
+view(dados)
 
 #1 Para este exercício, manipule os dados para considerar apenas os dados totais 
 #de cada indicador para cada região. 
 #Forneça o código que deixa os dados no seguinte formato:
 
-    #Approach **1**
-#Transpondo os dados da tabela
-dadost <- t(dados)
-#Realizando a visualização dos dados
-view(dadost)
-
-    #Approach **2**
-#Utilizando os dados da aula01 Vamos começar pivotando a tabela para deixa-la mais longa
-dados %>% 
-  pivot_longer(cols = -)
-
 #utilizando subset para remover algumas colunas desnecessárias na analise
-dados2 <- select(dados, -Jan, -Fev, -Mar, -Abr, -Mai, -Jun, -Jul, -Ago, -Set, -Out, -Nov, -Dez)
+dados2 <- select(dados, -jan, -fev, -mar, -abr, -mai, -jun, -jul, -ago, -set, -out, -nov, -dez)
 
 view(dados2)
-#Deixa a tabela mais larga
-dados2 %>%
-  pivot_wider(# pivota a tabela para deixa-la mais larga
-    names_from = type, # os nomes das colunas sao valores de type
-    values_from = count) # os valores das novas colunas sao os da coluna count
