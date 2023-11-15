3a3864c86a3b4becaf518fb230f56c2a5ecd76b2
MMCN <michelmconjaud@gmail.com>
2023-11-15 17:28:38 -0300

    Update_CorreçãoRmd
    Final com todas as descriçoes das componentes

--- AEM2.R
+++ AEM2.R
@@ -19,13 +19,16 @@ dados <- dados %>%
   rename_with(~ stri_trans_general(.x, "Latin-ASCII") %>% 
                 tolower())
 
-view(dados)
-
 #1 Para este exercício, manipule os dados para considerar apenas os dados totais 
 #de cada indicador para cada região. 
 #Forneça o código que deixa os dados no seguinte formato:
 
 #utilizando subset para remover algumas colunas desnecessárias na analise
 dados2 <- select(dados, -jan, -fev, -mar, -abr, -mai, -jun, -jul, -ago, -set, -out, -nov, -dez)
+
+dados2 %>%
+  pivot_wider(# pivota a tabela para deixa-la mais larga
+    names_from = c(ocorrencia, regiao), # os nomes das colunas sao valores de type
+    values_from = total) # os valores das novas colunas sao os da coluna count
 
 view(dados2)
